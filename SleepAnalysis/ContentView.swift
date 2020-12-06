6925b9f3abf61079c8745b6aeba611d82ac2f1b2
Minghui Zhang <congcong0716@sina.com>
2020-12-06 17:43:52 +0800

    Initial Commit

---
+++ SleepAnalysis/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SleepAnalysis
+//
+//  Created by 4paradigm on 2020/12/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SleepAnalysis/SleepAnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepAnalysisApp.swift
+//  SleepAnalysis
+//
+//  Created by 4paradigm on 2020/12/6.
+//
+
+import SwiftUI
+
+@main
+struct SleepAnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
